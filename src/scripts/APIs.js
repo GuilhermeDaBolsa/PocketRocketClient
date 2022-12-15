86b079173b195e05e445125490e147ad9fef9369
Guilherme Paini <guihdpaini@gmail.com>
2022-12-14 21:25:53 -0300

    improving error handling

--- src/scripts/APIs.js
+++ src/scripts/APIs.js
@@ -1,42 +1,71 @@
+// TODO: a lot of code repetition
+
 async function createUser() {
 	try{
 		const rawResponse = await fetch('http://localhost:8080/create_user', { method: "POST" });
-		return await rawResponse.json();
+		
+		if(rawResponse.ok)
+			return await rawResponse.json();
+
+		throw new Error(
+			await rawResponse.text()
+		);
 	} catch (error) {
 		return handleAPIError();
 	}
 }
 
 async function createRoom(userId) {
-	try{
-		const rawResponse = await fetch('http://localhost:8080/create_room', { method: "POST", body: JSON.stringify({userId}) });
-		return await rawResponse.json();
-	} catch (error) {
-		return handleAPIError();
-	}
+	return await fetchAPI('create_room', 'POST', JSON.stringify({userId}));
 }
 
 async function joinRoom(userId, roomId) {
 	try{
 		const rawResponse = await fetch('http://localhost:8080/join_room', { method: "POST", body: JSON.stringify({userId, roomId}) });
-		return await rawResponse.json();
+		
+		if(rawResponse.ok)
+			return await rawResponse.json();
+		
+		throw new Error(
+			await rawResponse.text()
+		);
 	} catch (error) {
-		return handleAPIError();
+		return handleAPIError(error);
 	}
 }
 
 async function getAllRooms() {
 	try{
 		const rawResponse = await fetch('http://127.0.0.1:8080/get_rooms', { method: "GET" });
-		return await rawResponse.json();
+
+		if(rawResponse.ok)
+			return await rawResponse.json();
+		
+		throw new Error(
+			await rawResponse.text()
+		);
 	} catch (error) {
 		return handleAPIError();
 	}
 }
 
+// just proposing a solution for code repetition
+async function fetchAPI(route, method, body = {}, headers = {}) {
+	try{
+		const rawResponse = await fetch(`http://127.0.0.1:8080/${route}`, { method : method, body: body, headers: headers});
+
+		if(rawResponse.ok)
+			return await rawResponse.json();
+		
+		throw new Error(await rawResponse.text());
+	} catch (error) {
+		return handleAPIError(error);
+	}
+}
+
 function handleAPIError(error) {
-	console.log(error);
-	return { isError: true, errorMessage: error?.msg ?? "IDK" }
+	console.error(error);
+	return { isError: true, errorMessage: error?.message ?? "Something went wrong!" }
 }
 
 export { createUser, createRoom, getAllRooms, joinRoom };
