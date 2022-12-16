e84faead08d33ef1312637956371c770f64591db
GuilhermeDaBolsa <Guilherme_Giordani@live.com>
2022-12-15 22:12:23 -0300

    sumarizing code of  APIs

--- src/scripts/APIs.js
+++ src/scripts/APIs.js
@@ -1,18 +1,5 @@
-// TODO: a lot of code repetition
-
 async function createUser() {
-	try{
-		const rawResponse = await fetch('http://localhost:8080/create_user', { method: "POST" });
-		
-		if(rawResponse.ok)
-			return await rawResponse.json();
-
-		throw new Error(
-			await rawResponse.text()
-		);
-	} catch (error) {
-		return handleAPIError();
-	}
+	return await fetchAPI('create_user', 'POST', {});
 }
 
 async function createRoom(userId) {
@@ -20,37 +7,14 @@ async function createRoom(userId) {
 }
 
 async function joinRoom(userId, roomId) {
-	try{
-		const rawResponse = await fetch('http://localhost:8080/join_room', { method: "POST", body: JSON.stringify({userId, roomId}) });
-		
-		if(rawResponse.ok)
-			return await rawResponse.json();
-		
-		throw new Error(
-			await rawResponse.text()
-		);
-	} catch (error) {
-		return handleAPIError(error);
-	}
+	return await fetchAPI('join_room', 'POST', JSON.stringify({userId, roomId}));
 }
 
 async function getAllRooms() {
-	try{
-		const rawResponse = await fetch('http://127.0.0.1:8080/get_rooms', { method: "GET" });
-
-		if(rawResponse.ok)
-			return await rawResponse.json();
-		
-		throw new Error(
-			await rawResponse.text()
-		);
-	} catch (error) {
-		return handleAPIError();
-	}
+	return await fetchAPI('get_rooms', 'GET');
 }
 
-// just proposing a solution for code repetition
-async function fetchAPI(route, method, body = {}, headers = {}) {
+async function fetchAPI(route, method, body, headers = {}) {
 	try{
 		const rawResponse = await fetch(`http://127.0.0.1:8080/${route}`, { method : method, body: body, headers: headers});
 
